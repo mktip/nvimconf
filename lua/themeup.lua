505acc39c78e9ca371df18580b843c7d4c2580d0
Mohammad Issa <mo.issa.ok@gmail.com>
2023-09-14 10:54:31 +0300

    add github theme

--- lua/themeup.lua
+++ lua/themeup.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.config = function()
-  vim.cmd([[ colorscheme catppuccin ]])
+  vim.cmd([[ colorscheme github_light_high_contrast ]])
   vim.o.background="light"
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
